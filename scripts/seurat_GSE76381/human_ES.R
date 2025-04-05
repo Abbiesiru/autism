deebfc8479b23e96fdfd0e0623a4a3c0c8206a9c
Abbie Wang <abbiesiru@gmail.com>
2025-04-04 21:29:03 -0700

    Rename mouse_neurons to human_ES & edit seurat_GSE104276

--- scripts/seurat_GSE76381/human_ES.R
+++ scripts/seurat_GSE76381/human_ES.R
@@ -1,34 +1,45 @@
-setwd("C:/Abbie/research/seurat/GSE104276/")
+setwd("C:/Abbie/research/seurat/GSE76381/")
 
 library(dplyr)
 library(Seurat)
 library(patchwork)
-library(readxl)
 library(Matrix)
 library(ggplot2)
 
-## example <- readRDS("GSE104276.CPM_example.rds") # saved seurat object
-
 #### 1. Setup the Seurat Object ####
 
-tmp1 <- read_excel("GSE104276_all_pfc_2394_UMI_count_NOERCC.xlsx")
-tmp1 <- as.data.frame(tmp1)
+raw <- read.table("GSE76381_ESMoleculeCounts.cef.txt", fill = TRUE) 
+
+### 1a. Create metadata ###
+
+metadata <- as.data.frame(t(raw[1:4, ]))
+rownames(metadata) <- metadata[, 2]
+colnames(metadata) <- metadata[1, ]
+metadata <- metadata[-1, ]
+
+### 1b. Create data matrix ###
+
+tmp1 <- raw
+colnames(tmp1) <- tmp1[2, ]
 rownames(tmp1) <- tmp1[, 1]
-tmp1 <- tmp1[, -1]
-tmp2 <- read.delim("cell_info.GSE104276.txt", header = TRUE, sep = "\t")
-matrix <- as.matrix(tmp1[, colnames(tmp1) %in% rownames(tmp2)])
-metadata <- tmp2[colnames(matrix), ]
+tmp1 <- tmp1[-(1:5), -1]
+tmp2 <- as.matrix(tmp1) # convert to char matrix
+matrix <- matrix(as.numeric(tmp2), 
+                 ncol = ncol(tmp2)) # convert to num matrix
+colnames(matrix) <- colnames(tmp1)
+rownames(matrix) <- rownames(tmp1)
+
+### 1c. Create seurat obj ###
 
 seurat_obj <- CreateSeuratObject(
   counts = as(matrix, "dgCMatrix"),
   meta.data = metadata,
-  project = "GSE104276"
+  project = "human_ES"
 )
 # An object of class Seurat 
-# 24153 features across 2391 samples within 1 assay 
-# Active assay: RNA (24153 features, 0 variable features)
+# 18539 features across 1715 samples within 1 assay 
+# Active assay: RNA (18539 features, 0 variable features)
 # 1 layer present: counts
-
 
 #### 2. Standard pre-processing workflow ####
 
@@ -82,17 +93,17 @@ seurat_obj <- RunPCA(seurat_obj, features = VariableFeatures(object = seurat_obj
 # Examine and visualize PCA results a few different ways
 print(seurat_obj[["pca"]], dims = 1:5, nfeatures = 5)
 VizDimLoadings(seurat_obj, dims = 1:2, reduction = "pca")
-ggsave(filename = "output/scale_and_PCA/GSE104276_VizDimLoadings.jpg", height = 7, width = 12, quality = 50)
+ggsave(filename = "output/scale_and_PCA/human_ES_VizDimLoadings.jpg", height = 7, width = 12, quality = 50)
 DimPlot(seurat_obj, reduction = "pca") + NoLegend()
-ggsave(filename = "output/scale_and_PCA/GSE104276_DimPlot_PCA.jpg", height = 7, width = 12, quality = 50)
+ggsave(filename = "output/scale_and_PCA/human_ES_DimPlot_PCA.jpg", height = 7, width = 12, quality = 50)
 DimHeatmap(seurat_obj, dims = 1:15, cells = 500, balanced = TRUE)
-ggsave(filename = "output/scale_and_PCA/GSE104276_DimHeatmap.jpg")
+ggsave(filename = "output/scale_and_PCA/human_ES_DimHeatmap.jpg")
 
 
 #### 7. Determine the ‘dimensionality’ of the dataset ####
 
 ElbowPlot(seurat_obj)
-ggsave(filename = "output/scale_and_PCA/GSE104276_ElbowPlot.jpg", height = 7, width = 12, quality = 50)
+ggsave(filename = "output/scale_and_PCA/human_ES_ElbowPlot.jpg", height = 7, width = 12, quality = 50)
 
 #### 8. Cluster the cells ####
 
@@ -107,8 +118,8 @@ head(Idents(seurat_obj), 5)
 
 seurat_obj <- RunUMAP(seurat_obj, dims = 1:10)
 DimPlot(seurat_obj, reduction = "umap")
-ggsave(filename = "output/clustering/GSE104276_DimPlot_UMAP.jpg", height = 7, width = 12, quality = 50)
-saveRDS(seurat_obj, file = "output/saved_seurat_obj/GSE104276_seurat_obj")
+ggsave(filename = "output/clustering/human_ES_DimPlot_UMAP.jpg", height = 7, width = 12, quality = 50)
+saveRDS(seurat_obj, file = "output/saved_seurat_obj/human_ES_seurat_obj")
 
 
 #### 10. Finding differentially expressed features (cluster biomarkers) ####
@@ -119,21 +130,21 @@ obj.markers <- FindAllMarkers(seurat_obj, only.pos = TRUE)
 obj.markers %>%
   group_by(cluster) %>%
   dplyr::filter(avg_log2FC > 1)
-# A tibble: 7,244 × 7
-# Groups:   cluster [9]
-#         p_val avg_log2FC pct.1 pct.2 p_val_adj cluster gene     
-#         <dbl>      <dbl> <dbl> <dbl>     <dbl> <fct>   <chr>    
-#   1 3.04e-104       1.11 0.895 0.592 4.16e-100 0       LDHB     
-# 2 1.10e- 81       2.35 0.432 0.111 1.51e- 77 0       CCR7     
-# 3 4.20e- 79       1.10 0.848 0.407 5.75e- 75 0       CD3D     
-# 4 2.53e- 48       2.12 0.336 0.108 3.46e- 44 0       PRKCQ-AS1
-# 5 1.40e- 47       1.21 0.625 0.358 1.91e- 43 0       NOSIP    
-# 6 3.99e- 41       1.93 0.316 0.109 5.47e- 37 0       LEF1     
-# 7 5.75e- 37       1.37 0.42  0.188 7.89e- 33 0       PIK3IP1  
-# 8 1.12e- 32       2.42 0.185 0.045 1.53e- 28 0       FHIT     
-# 9 3.67e- 32       1.88 0.259 0.087 5.03e- 28 0       MAL      
-# 10 2.99e- 30       2.23 0.155 0.033 4.11e- 26 0       NELL2    
-# # ℹ 7,234 more rows
+# A tibble: 9,652 × 7
+# # Groups:   cluster [9]
+# p_val avg_log2FC pct.1 pct.2 p_val_adj cluster gene 
+# <dbl>      <dbl> <dbl> <dbl>     <dbl> <fct>   <chr>
+#   1 2.18e-145       3.26 0.802 0.137 4.04e-141 0       NFIA 
+# 2 5.76e- 88       1.39 0.571 0.098 1.07e- 83 0       TTR  
+# 3 2.64e- 87       2.86 0.559 0.094 4.89e- 83 0       HTR2C
+# 4 4.49e- 83       3.01 0.58  0.124 8.32e- 79 0       EPHA3
+# 5 1.57e- 79       3.17 0.485 0.077 2.91e- 75 0       ZIC4 
+# 6 1.75e- 71       1.96 0.846 0.436 3.24e- 67 0       SLIT2
+# 7 3.32e- 70       3.14 0.414 0.056 6.15e- 66 0       LRP1B
+# 8 4.35e- 70       1.77 0.923 0.8   8.06e- 66 0       SPARC
+# 9 5.39e- 70       2.07 0.846 0.482 9.99e- 66 0       WLS  
+# 10 1.76e- 68       2.09 0.861 0.517 3.25e- 64 0       FOS  
+# # ℹ 9,642 more rows
 # # ℹ Use `print(n = ...)` to see more rows
 
 obj.markers %>%
