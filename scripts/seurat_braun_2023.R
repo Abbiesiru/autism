e6305bce7ed47e8782db15a6169d6f91cb68b8ac
Abbie Wang <abbiesiru@gmail.com>
2025-06-11 22:15:31 -0700

    Edit seurat_obj

--- scripts/seurat_braun_2023.R
+++ scripts/seurat_braun_2023.R
@@ -1,17 +1,44 @@
 setwd("C:/Abbie/research/seurat/braun_2023/")
 
-library(dplyr)
 library(Seurat)
-library(patchwork)
+library(Matrix)
 library(ggplot2)
-library(SeuratDisk)
-library(zellkonverter)
 
+# Load the sparse matrix, genes & barcodes
+expr_matrix <- readMM("cellranger_output_2/matrix.mtx") 
+expr_matrix <- t(expr_matrix)
+genes <- read.delim("cellranger_output_2/features.tsv", header = FALSE)
+barcodes <- read.delim("cellranger_output_2/barcodes.tsv", header = FALSE)
 
+rownames(expr_matrix) <- genes$V2   # set gene names as row names
+colnames(expr_matrix) <- barcodes$V1 # set cell barcodes as col names
 
-#### 1. Setup the Seurat Object ####
+seurat_obj <- CreateSeuratObject(counts = expr_matrix)
 
-# Load file
-Convert("human_dev_GRCh38-3.0.0.h5ad", dest = "h5seurat", overwrite = TRUE)
-zellkonverter::readH5AD("human_dev.h5ad", use_hdf5 = FALSE)
-seurat_obj <- Read10X_h5("human_dev_GRCh38-3.0.0.h5seurat")
+cell_anno <- read.delim("cell_anno.txt", header = TRUE, sep = "\t")
+
+# Set rownames as barcodes to align with Seurat cells
+rownames(cell_anno) <- cell_anno$Cell_name
+
+# Subset to cells in Seurat object
+cell_anno <- cell_anno[colnames(seurat_obj), , drop = FALSE]
+
+# Add annotation as metadata
+seurat_obj <- AddMetaData(seurat_obj, metadata = cell_anno)
+
+# Extract the t-SNE coords from metadata as a matrix
+colnames(cell_anno)[colnames(cell_anno) == "tSNE_x"] <- "tSNE_1"
+colnames(cell_anno)[colnames(cell_anno) == "tSNE_y"] <- "tSNE_2"
+tsne_coords <- as.matrix(cell_anno[, c("tSNE_1", "tSNE_2")])
+
+# Create a new DimReduc object
+seurat_obj[["tSNE_plot"]] <- CreateDimReducObject(
+  embeddings = tsne_coords,
+  key = "TSNE_",
+  assay = DefaultAssay(seurat_obj)
+)
+
+ggplot(cell_anno, aes(x = tSNE_1, y = tSNE_2, color = Region)) +
+  geom_point() +
+  theme_minimal()
+
