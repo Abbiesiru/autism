16977d8b69d9be3c2a0c5e4c25339c130650255d
Abbiesiru <abbiesiru@gmail.com>
2025-07-07 11:57:52 -0400

    Update braun_combine_seurat_obj.R

--- scripts/braun_combine_seurat_obj.R
+++ scripts/braun_combine_seurat_obj.R
@@ -4,35 +4,21 @@ tmp <- read.csv("/home/abbiew/single_cell/autism_risk_genes_combined.csv", sep =
 asd_risk_genes <- tmp$Gene
 
 library(Seurat)
-library(AUCell)
-
-cell_rankings_list <- list()
 
 process_and_subset <- function(path, genes) {
   seurat_obj <- readRDS(path)
 
-  # 1. rank the genes in each cell, subset & export
-  expr_mat <- GetAssayData(object = seurat_obj, assay = "RNA", slot = "counts")
-  cell_rankings <- AUCell_buildRankings(
-    expr_mat,
-    plotStats = FALSE,
-    keepZeroesAsNA = FALSE,
-    verbose = TRUE
-  )
-  cell_rankings_subset <- getRanking(cell_rankings)[rownames(cell_rankings) %in% asd_risk_genes, ]
-  cell_rankings_list[[basename(path)]] <<- cell_rankings_subset
-
-  # 2. log2 transformation
+  # 1. log2 transformation
   seurat_obj@assays$RNA@layers$data@x <- log2(seurat_obj@assays$RNA@layers$data@x + 1)
   Idents(seurat_obj, cells = seurat_obj$Cell_name) <- seurat_obj$Cell_type_raw
 
-  # 3. add tsne coordinates
+  # 2. add tsne coordinates
   temp_data <- as.matrix(seurat_obj@meta.data[, c("tSNE_x", "tSNE_y")])
   colnames(temp_data) <- c("tSNE_1", "tSNE_2");
   seurat_obj[["tsne"]] <- CreateDimReducObject(embeddings = temp_data, key = "tSNE_",
                                                     assay = DefaultAssay(seurat_obj))
 
-  # 4. subset to ASD risk genes only 
+  # 3. subset to ASD risk genes only 
   seurat_obj <- seurat_obj[rownames(seurat_obj) %in% asd_risk_genes, ]
 
   return(seurat_obj)
@@ -40,9 +26,5 @@ process_and_subset <- function(path, genes) {
 
 processed_list <- lapply(seurat_paths, process_and_subset, genes = asd_risk_genes)
 combined_seurat <- merge(x = processed_list[[1]], y = processed_list[-1])
-
-merged_cell_rankings <- do.call(cbind, cell_rankings_list)
-merged_cell_rankings <- 1 - (merged_cell_rankings / 26261 * 100)
-saveRDS(merged_cell_rankings, file = "cell_rankings_combined.rds")
 
 saveRDS(combined_seurat, "seurat_obj_subset_combined.rds")
